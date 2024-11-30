bc5338e77f28d70d50a9cef73c29ad00b9af5cb9
Israel <ioliveirap90@gmail.com>
2024-11-30 14:58:34 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Guess the Number Game
+## Technologies used
+
+![N|Solid](https://img.shields.io/badge/HTML-239120?style=for-the-badge&logo=html5&logoColor=white) ![N|Solid](https://img.shields.io/badge/CSS-239120?&style=for-the-badge&logo=css3&logoColor=white) ![N|Solid](https://img.shields.io/badge/JavaScript-F7DF1E?style=for-the-badge&logo=javascript&logoColor=black)
+
+# About
+This is a simple guessing game where a random number is generated and you have to guess which one is it by receveing clues after each attempt.
+
+## Features
+
+- Html and CSS to provide the front-end [vercel](https://jogo-lime-beta.vercel.app/).
+- Javascript for the user interaction with the game.
+
+## Author
+Israel
---
+++ app.js
@@ -0,0 +1,84 @@
+let drawnNumbers = [];
+let limitNumber = 200;
+let secretNumber = randomSecretNumber(1, limitNumber);
+let guesses = 1;
+// Function to compare if the input number is the secret number and give tips so the user can discover the number.
+function checkGuess() {
+   let guess = Math.floor(document.querySelector('input').value);
+   if(secretNumber == guess) {
+        modifyHtml('h1', 'Parabéns você acertou!!')
+        let tries = guesses > 1 ? 'tentativas' : "tentativa";
+        let winnerMessage = `Você descobriu o número secreto em ${guesses} ${tries}`;
+        modifyHtml('p', winnerMessage);
+        // Enabling new game button on the page.
+        document.getElementById('reiniciar').removeAttribute('disabled');
+   }    
+   else {
+        if(guess > secretNumber) {
+            modifyHtml('p', 'O número secreto é menor');           
+        }
+        else {
+            modifyHtml('p', 'O número secreto é maior');
+        }
+        guesses++;
+        clearInput()
+   }
+}
+// Function to change html values using js
+function modifyHtml(tag, valor) {
+    let campo = document.querySelector(tag);
+    campo.innerHTML = valor;
+    // Script in line 7 of html file to be able to use this variation
+    responsiveVoice.speak(valor, 'Brazilian Portuguese Female', {rate:1.2});
+
+    // Native js instead
+    /*if ('speechSynthesis' in window) {        
+        let utterance = new SpeechSynthesisUtterance(valor);
+        utterance.lang = 'pt-BR'; 
+        utterance.rate = 1.2; 
+        window.speechSynthesis.speak(utterance); 
+        console.log('here');
+    }
+    else {
+        console.log("Web Speech API não suportada neste navegador.");
+    }*/
+}
+// Function that generates a random number for our game using Math.floor instead of parseInt because is faster
+function randomSecretNumber(min, max) {
+    let drawnNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+    if(drawnNumbers.includes(drawnNumber)) {
+        // Recursive in case the number was already drawn
+        return randomSecretNumber(1, limitNumber);
+    }
+    else {
+        // Adding the drawn number to the array
+        drawnNumbers.push(drawnNumber);
+        return drawnNumber;
+    }
+}
+// Clearing the input field
+function clearInput() {
+    guess = document.querySelector('input');
+    guess.value = '';
+}
+// After a win start a new game
+function newGame() {
+    secretNumber = randomSecretNumber(1, limitNumber);
+    clearInput();
+    guesses = 1;    
+    initialMessage();
+    // Adding attribute to make the new game button disabled whenever starting after winning
+    document.getElementById('reiniciar').setAttribute('disabled', true);
+    // Don't allow the previous 10 numbers to be drawn in the next game then clear after
+    if(drawnNumbers.length > 10) {
+        drawnNumbers = [];
+    }
+}
+
+function initialMessage() {
+    // Modifying some html tags
+    modifyHtml('h1', 'Adivinhe o número');
+    modifyHtml('p', `Escolha um número entre 1 e ${limitNumber}`);
+}
+
+initialMessage();
